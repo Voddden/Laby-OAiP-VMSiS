0c2e3c131f7c9ee30c6df06824956fa3cf02cba1
Даник <voddden@gmail.com>
2022-09-21 18:49:36 +0300

    ValidationLib: полностью реализовал валидацию вещественных чисел. Сейв перед чисткой кода

--- Lab-2/ValidationLib/inputReal.cpp
+++ Lab-2/ValidationLib/inputReal.cpp
@@ -25,12 +25,12 @@ double Pow(long num, long deg) {
 
 double parseFractPartToDouble(const char* str) {
 	int g = 0;
-	while (str[g] != '.' && str[g] != ',' && str[g] != '\0') ++g; // g == кол-во цифр в целой части
-	int whole_digits = g;
+	while (str[g] != '.' && str[g] != ',' && str[g] != '\0') ++g;
+	int whole_digits = g; // кол-во цифр в целой части
 	int u = g;
-	for (; str[u] != '\0'; ++u); // u == кол-во цифр вообще в числе
-	int all_digits = u - 1;
-	int fract_digits = all_digits - whole_digits;
+	for (; str[u] != '\0'; ++u); 
+	int all_digits = u - 1; // кол-во цифр вообще в числе
+	int fract_digits = all_digits - whole_digits; // кол-во цифр в дробной части
 
 	int arr_int_size = fract_digits;
 	int* arr_int = (int*)malloc(arr_int_size * sizeof(int));
@@ -67,14 +67,31 @@ double parseWholePartToDouble(const char* str) {
 	return result;
 }
 
+bool inputIsInvalidDueToDigitAbsence(const char* str) {
+	int num_of_digits = 0;
+	for (int i = 0; str[i] != '\0'; ++i) {
+		if (str[i] == '0' || str[i] == '1' || str[i] == '2' ||
+			str[i] == '3' || str[i] == '4' || str[i] == '5' ||
+			str[i] == '6' || str[i] == '7' || str[i] == '8' ||
+			str[i] == '9')
+		{
+			++num_of_digits;
+		}
+	}
+	if (num_of_digits == 0)
+		return true;
+	else
+		return false;
+}
+
 bool inputIsInvalidDueToMinuses(const char* str) {
-	for (int i = 1; i != '\0'; ++i) {
+	for (int i = 1; str[i] != '\0'; ++i) {
 		if (str[i] == '-')
 			return true;
 	}
 	return false;
 }
-
+/*
 bool inputIsInvalidDueToSpaces(const char* str) {
 	for (int i = 0; str[i] != '\0'; ++i) {
 		if (str[i] == ' ')
@@ -82,7 +99,8 @@ bool inputIsInvalidDueToSpaces(const char* str) {
 	}
 	return false;
 }
-
+избыточная проверка
+*/
 bool inputIsInvalidDueToSeparators(const char* str) {
 	int number_of_separators = 0;
 	for (int i = 0; str[i] != '\0'; ++i)
@@ -97,7 +115,7 @@ bool inputIsInvalidDueToLetters(const char* str) {
 		if (str[i] != '0' && str[i] != '1' && str[i] != '2' &&
 			str[i] != '3' && str[i] != '4' && str[i] != '5' &&
 			str[i] != '6' && str[i] != '7' && str[i] != '8' &&
-			str[i] != '9' && str[i] != '.' && str[i] != ',' //&& str[i] != '-'
+			str[i] != '9' && str[i] != '.' && str[i] != ',' && str[i] != '-'
 			)
 		{
 			return true;
@@ -107,26 +125,31 @@ bool inputIsInvalidDueToLetters(const char* str) {
 }
 
 double inputReal(const char* prompt) {
-	char input[length];
+	char input[length] = {};
 
 	printf("%s", prompt);
-	scanf_s("%s", input, length);
+	gets_s(input);
+	//scanf_s("%s", input, length);
 
 	// Вызов проверяющих функций
-	while (inputIsInvalidDueToLetters(input) || inputIsInvalidDueToSeparators(input)// || inputIsInvalidDueToMinuses(input) || inputIsInvalidDueToSpaces(input)
+	while (inputIsInvalidDueToLetters(input) || inputIsInvalidDueToSeparators(input) || inputIsInvalidDueToMinuses(input) || inputIsInvalidDueToDigitAbsence(input)
 		) 
 	{
-		// некорректные данные. Повторный ввод
-		printf("Error. %s", prompt);
-		fflush(stdin);
-		scanf_s("%s", input, length);
+		//fflush(stdin); 
+		system("cls");
+		printf("Error. Try again %s", prompt);
+		gets_s(input);
+		//scanf_s("%s", input, length);
 	}
 
-	char input_unsigned[length + 1];
+	char input_unsigned[length] = {};
+	bool numIsSigned = false;
+
 	if (input[0] == '-') {
-		for (int i = 0; i < length - 1; ++i) {
+		for (int i = 0; i < length-1; ++i) {
 			input_unsigned[i] = input[i + 1];
 		}
+		numIsSigned = true;
 	}
 	else {
 		for (int i = 0; i < length; ++i) {
@@ -135,13 +158,13 @@ double inputReal(const char* prompt) {
 	}
 
 	double res = 0;
-	// парсинг целой части
-	res += parseWholePartToDouble(input);
-	// парсинг дробной части
-	res += parseFractPartToDouble(input);
-	//установление необходимого знака
-
+	// парсинг целой части:
+	res += parseWholePartToDouble(input_unsigned);
+	// парсинг дробной части:
+	res += parseFractPartToDouble(input_unsigned);
+	//установление знака:
+	if (numIsSigned)
+		res *= -1;
 
 	return res;
-	
 }
