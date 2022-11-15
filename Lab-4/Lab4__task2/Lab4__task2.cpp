6c46d715691a6b0ebe48fc1ddddd391cfccd55e6
Даник <voddden@gmail.com>
2022-11-16 02:05:26 +0300

    ЛР4, таска 2 в порядке

--- Lab-4/Lab4__task2/Lab4__task2.cpp
+++ Lab-4/Lab4__task2/Lab4__task2.cpp
@@ -33,57 +33,72 @@ void randArr(int** arr, int m, int n) {
 void outputArr(int** arr, int m, int n) {
 	for (int i = 0; i < m; ++i) {
 		for (int j = 0; j < n; ++j) {
-			printf("%d ", arr[i][j]);
+			printf("%5d ", arr[i][j]);
 		}
 		printf("\n");
 	}
 }
+// задание:
 
-void swap(int& a, int& b) {
-	int temp = a;
-	a = b;
-	b = temp;
+bool rowDontHasNull(int* arr, int size) {
+	for (int i = 0; i < size; ++i) 
+		if (arr[i] == 0)
+			return false;
+	return true;
 }
 
-void swapRows(int* a, int* b, int n) {
-	int* temp = (int*)malloc(sizeof(int) * n);
-	for (int i = 0; i < n; ++i) {
-		temp[i] = a[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		a[i] = b[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		b[i] = temp[i];
-	}
-
+bool allStringsHaveNull(int** arr, int m, int n) {
+	for (int i = 0; i < m; ++i)
+		if (rowDontHasNull(arr[i], n))
+			return false;
+	return true;
 }
+
+void nullAllNegative(int** arr, int m, int n) {
+	for (int i = 0; i < m; ++i)
+		for (int j = 0; j < n; ++j)
+			if (arr[i][j] < 0)
+				arr[i][j] = 0;
+}
+
+/* B-12
+Проверить, все ли строки матрицы содержат хотя бы один нулевой элемент. 
+Если нет, то заменить значения всех отрицательных элементов матрицы на нулевые.
+*/
 
 void main() 
 {
-	int arr1[3][4] = {8, -1, 0, -4,
-					  2,  3, 2, -1,
-					  -3, 1, 0,  9 };
+	srand(time(NULL));
+	setlocale(LC_ALL, "ru");
+	printf("ЛР 4, таска 2\n\n");
+	printf("Матрица m x n\n");
 
-	int* a;
-	int* b;
-	int n = 4;
+	int m = inputNatural("m = ");
+	int n = inputNatural("n = ");
 
-	int* temp = (int*)malloc(sizeof(int) * n);
-	for (int i = 0; i < n; ++i) {
-		temp[i] = a[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		a[i] = b[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		b[i] = temp[i];
+	int** arr = allocation(m, n);
+
+	int choice = inputNatural("---Заполнить матрицу с клавиатуры - 1\tслучайными значениями - 2\n");
+	while (choice != 1 && choice != 2) {
+		choice = inputNatural("Error\n---Заполнить матрицу с клавиатуры - 1\tслучайными значениями - 2\n");
 	}
 
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			printf("%d ", arr1[i][j]);
-		}
-		printf("\n");
+	switch (choice) {
+	case 1:
+		inputArr(arr, m, n); break;
+	case 2:
+		randArr(arr, m, n);
+	}
+
+	printf("\nМатрица:\n\n");
+	outputArr(arr, m, n);
+	// работа с массивом:
+	if (allStringsHaveNull(arr, m, n)) {
+		printf("\nДействия не требуются\n");
+	}
+	else {
+		nullAllNegative(arr, m, n);
+		printf("\nОбработанная матрица:\n\n");
+		outputArr(arr, m, n);
 	}
 }
