54fa6efc2d09ebcd8c2faff11039731a014d54f4
Даник <voddden@gmail.com>
2022-11-16 19:47:44 +0300

    Lab5_task1 edit

--- Lab-5/Lab5__task1/Lab5__task1.cpp
+++ Lab-5/Lab5__task1/Lab5__task1.cpp
@@ -1,7 +1,40 @@
-﻿#include <stdio.h>
-#include "..\..\ValidationLib\inputNatural.h"
+﻿#include <stdlib.h>
+#include <stdio.h>
 
-void main() {
-	int a = inputNatural("a = ");
-	printf("your number: %d", a);
+void shift(int* arr, int size, int Ineedless) {
+	for (int i = Ineedless; i < size; ++i) {
+		arr[i] = arr[i + 1];
+	}
+}
+
+void deleteNull(int* arr, int& size) {
+	int newSize = size;
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] == 0) {
+			shift(arr, size, i);
+			--newSize;
+			--i;
+		}
+	}
+	realloc(arr, newSize);
+	size = newSize;
+}
+
+void main()
+{
+	int size = 12;
+	int* arr = (int*)malloc(size * sizeof(int));
+
+
+	for (int i = 0; i < size; ++i)
+		scanf_s("%d", &arr[i]);
+
+	deleteNull(arr, size);
+
+	printf("\n");
+	for (int i = 0; i < size; ++i) {
+		printf("%d\n", arr[i]);
+	}
+
+
 }
